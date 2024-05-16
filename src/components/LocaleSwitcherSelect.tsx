f07c1bc447a03be241a71cad0637a295ebc39c00
Matthias Grieder <matthias.grieder@snapscreen.com>
2024-05-16 16:04:49 +0200

    modified:   src/components/LocaleSwitcherSelect.tsx

--- src/components/LocaleSwitcherSelect.tsx
+++ src/components/LocaleSwitcherSelect.tsx
@@ -50,7 +50,7 @@ export default function LocaleSwitcherSelect({
       >
         {children}
       </select>
-      <span className="pointer-events-none absolute right-2 top-[8px]">
+      <span className="pointer-events-none absolute right-0 top-[12px]">
         <svg className="h-6 w-6" fill="currentColor" viewBox="0 -960 960 960">
           <path d="M480-344 240-584l43-43 197 197 197-197 43 43-240 240Z" />
         </svg>
